0aa8926ab706a9cd395fe885c14849bf7ee8371a
Michael Leontiev <michlea@yandex.ru>
2023-02-26 18:05:40 +0400

    feat: add ClothItem model

--- backend/api/restapi/app/models.py
+++ backend/api/restapi/app/models.py
@@ -29,3 +29,32 @@ class WeatherRecord(models.Model):
         constraints = [
             models.UniqueConstraint(fields=["day", "city"], name="unique_day_city")
         ]
+
+
+class ClothItem(models.Model):
+    class ClothTypes(models.IntegerChoices):
+        OUTERWEAR = 1, "Верхняя одежда"
+        HEADGEAR = 2, "Головной убор"
+        SOCKS = 3, "Чулочно-носочные изделия"
+        GLOVES = 4, "Перчаточные изделия"
+        SCARFS = 5, "Шарфы/платки"
+        SHOES = 6, "Обувь"
+
+    name = models.CharField("Название одежды", max_length=200)
+    image_url = models.URLField("Ссылка на изображение", blank=True, null=True)
+    temperature_min = models.IntegerField("Минимальная температура")
+    temperature_max = models.IntegerField("Максимальная температура")
+    type = models.IntegerField("Тип одежды", choices=ClothTypes.choices)
+    thermal_resistence = models.FloatField("Тепловая устойчивость")
+
+    def __str__(self):
+        return self.name
+
+    def clean(self) -> None:
+        super().clean()
+        if self.temperature_min >= self.temperature_max:
+            raise self.ValidationError("Максимальная температура должна быть больше минимальной")
+
+    class Meta:
+        verbose_name = "Элемент одежды"
+        verbose_name_plural = "Элементы одежды"
