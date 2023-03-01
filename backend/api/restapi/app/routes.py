bcd6e633d68588ee97e3c9747fddc9f25fb8ff60
IgnatInyutsin <IgnatInyutsin>
2023-03-01 16:48:25 +0400

    добавил в routes

--- backend/api/restapi/app/routes.py
+++ backend/api/restapi/app/routes.py
@@ -1,10 +1,11 @@
 from django.urls import include, path
 from rest_framework import routers
-from restapi.app.views import WeatherViewSet
+from restapi.app.views import WeatherViewSet, ClothViewSet
 
 #устанавливаем пути
 router = routers.DefaultRouter()
 router.register('weather', WeatherViewSet)
+router.register("clothes", ClothViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
